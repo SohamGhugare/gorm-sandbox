971695ed4ecba1710721cab861c2d967055e4f78
SohamGhugare <soham.ghugare@gmail.com>
2023-04-29 10:38:45 +0530

    Added creatProduct function

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -21,5 +23,21 @@ func main() {
 
 	// Migrating schema
 	db.AutoMigrate(&Product{})
+
+	id, err := createProduct(db, "D32", 5)
+	fmt.Printf("Product #%d created successfully", id)
+}
+
+// Creating new product
+func createProduct(db *gorm.DB, code string, price uint) (uint, error) {
+	product := Product{
+		Code:  code,
+		Price: price,
+	}
+	result := db.Create(&product)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+	return product.ID, nil
 
 }
